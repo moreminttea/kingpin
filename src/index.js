6024b726c47e6a3242837fdbfd948351df5d91a3
moreminttea <97208400+moreminttea@users.noreply.github.com>
2022-02-12 18:34:45 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter } from "react-router-dom";
 
-ReactDOM.render((
-  <HashRouter basename="/">
-    <App/>
-  </HashRouter>),
+ReactDOM.render(
+  <App/>,
   document.getElementById('root')
 );
 
