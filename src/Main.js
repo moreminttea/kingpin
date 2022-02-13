0849c227588bb7a77571724ce0f4818def1621ac
moreminttea <97208400+moreminttea@users.noreply.github.com>
2022-02-12 18:44:26 -0800

    Create a React app and deploy it to GitHub Pages

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Routes ,Route } from 'react-router-dom';
-
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Kingpin from './pages/Kingpin';
 import Incarnate from './pages/Incarnate';
@@ -8,12 +7,16 @@ import About from './pages/About';
 
 const Main = () => {
   return (
-    <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path="/kingpin" element={<Kingpin/>} />
-        <Route path="/incarnate" element={<Incarnate/>} />
-        <Route path="/about" element={<About/>} />
-    </Routes>
+    <div>
+    <HashRouter basename="/">
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path="/kingpin" element={<Kingpin/>} />
+          <Route path="/incarnate" element={<Incarnate/>} />
+          <Route path="/about" element={<About/>} />
+      </Routes>
+    </HashRouter>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>),
+ReactDOM.render(
+  <App/>,
   document.getElementById('root')
 );
 
