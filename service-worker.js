eba7c26b7c8594cdde99aa8b6434599207c20a47
moreminttea <97208400+moreminttea@users.noreply.github.com>
2022-07-19 23:40:47 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kingpin/precache-manifest.f7140b37a6cb7cfdb1acf482de5e0cfc.js"
+  "/kingpin/precache-manifest.baa4949475caeeaacde575b6927b47e8.js"
 );
 
 self.addEventListener('message', (event) => {
