343c4f5f9a2802b1539b6a4f94e7d3bc6c0f4310
moreminttea <97208400+moreminttea@users.noreply.github.com>
2024-11-16 23:52:34 -0800

    Updates

---
+++ precache-manifest.90ebec61c52908e5913b83f50f45b709.js
@@ -0,0 +1,554 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019a585700ecb34c24d2c9b0d73123d8",
+    "url": "/kingpin/index.html"
+  },
+  {
+    "revision": "413c12be776418475e75",
+    "url": "/kingpin/static/css/2.0ef3f78d.chunk.css"
+  },
+  {
+    "revision": "40f12112275ae84d7b41",
+    "url": "/kingpin/static/css/main.8be4edc6.chunk.css"
+  },
+  {
+    "revision": "413c12be776418475e75",
+    "url": "/kingpin/static/js/2.44302672.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/kingpin/static/js/2.44302672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f12112275ae84d7b41",
+    "url": "/kingpin/static/js/main.bb987e92.chunk.js"
+  },
+  {
+    "revision": "c12b79d825cce587fbe8",
+    "url": "/kingpin/static/js/runtime-main.d1a3c5fe.js"
+  },
+  {
+    "revision": "97ce121b0118800cd23b76c0981715a7",
+    "url": "/kingpin/static/media/alcerium.97ce121b.svg"
+  },
+  {
+    "revision": "3952ef03f714c7fce3e4ef93aa9b4be9",
+    "url": "/kingpin/static/media/alcerium_icon.3952ef03.png"
+  },
+  {
+    "revision": "2a6708263121cc7e7b6daac5f9558b97",
+    "url": "/kingpin/static/media/alcerium_iconRed.2a670826.png"
+  },
+  {
+    "revision": "29c97321f0fd99f53e5bab9709d41ba6",
+    "url": "/kingpin/static/media/alcerium_iconWhite.29c97321.png"
+  },
+  {
+    "revision": "42d2ca05f6e824369d5e3381beb24290",
+    "url": "/kingpin/static/media/angelfall.42d2ca05.png"
+  },
+  {
+    "revision": "a61bde20c71566afcb81d2f622808e81",
+    "url": "/kingpin/static/media/arkenal.a61bde20.png"
+  },
+  {
+    "revision": "43659d25113b31e80ff94c0d1fbddbc0",
+    "url": "/kingpin/static/media/arrow.43659d25.svg"
+  },
+  {
+    "revision": "cc7c12ee24d153661614bb6b77c293d1",
+    "url": "/kingpin/static/media/bazaar_of_kirci.cc7c12ee.png"
+  },
+  {
+    "revision": "398b5baf3e0a3c8f68b4664e1d4a6861",
+    "url": "/kingpin/static/media/brighte_card.398b5baf.png"
+  },
+  {
+    "revision": "483f13dd5473b5c064faad56a3a5718b",
+    "url": "/kingpin/static/media/brighte_fullbody.483f13dd.png"
+  },
+  {
+    "revision": "d033f9acfaedfc3ee1e0874b4862123e",
+    "url": "/kingpin/static/media/brighte_icon.d033f9ac.png"
+  },
+  {
+    "revision": "042ac12d98469dfb0122ceb8d681812a",
+    "url": "/kingpin/static/media/brighte_icon_back.042ac12d.svg"
+  },
+  {
+    "revision": "ce3a40157fff0e0454ccf312a8074f5b",
+    "url": "/kingpin/static/media/castorix.ce3a4015.svg"
+  },
+  {
+    "revision": "f1881e294ef16a6c823012ae6f2ce36e",
+    "url": "/kingpin/static/media/castorix_icon.f1881e29.png"
+  },
+  {
+    "revision": "47e192aed08d1954ef4371986e309b09",
+    "url": "/kingpin/static/media/castorix_iconRed.47e192ae.png"
+  },
+  {
+    "revision": "6dd0e4d1db93e081ca89b7092f301f66",
+    "url": "/kingpin/static/media/castorix_iconWhite.6dd0e4d1.png"
+  },
+  {
+    "revision": "9966d35fdd9a96bac202f1d66ae24e93",
+    "url": "/kingpin/static/media/champions_circle.9966d35f.png"
+  },
+  {
+    "revision": "39c0444d509698a4f48b9e020e955ec3",
+    "url": "/kingpin/static/media/cometere.39c0444d.png"
+  },
+  {
+    "revision": "801f8addd5ee4d64b0bd2dda8e1842e4",
+    "url": "/kingpin/static/media/depths_of_valerim.801f8add.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/drach_card.1360c3dd.png"
+  },
+  {
+    "revision": "468d587cf4735dd0dbf6d9ecc95d7604",
+    "url": "/kingpin/static/media/drach_fullbody.468d587c.png"
+  },
+  {
+    "revision": "7cd1d9ab55e587c9bd24e6edb1880408",
+    "url": "/kingpin/static/media/drach_icon.7cd1d9ab.png"
+  },
+  {
+    "revision": "a08f89ae6878cc19ad0bc8524c74c1ef",
+    "url": "/kingpin/static/media/drach_icon_back.a08f89ae.svg"
+  },
+  {
+    "revision": "13b86fa6b02b10c43f56a3cd0f1672ca",
+    "url": "/kingpin/static/media/excelia.13b86fa6.png"
+  },
+  {
+    "revision": "9a5a7bc1143f618d8bf92d0e1b388b3b",
+    "url": "/kingpin/static/media/glass1.9a5a7bc1.svg"
+  },
+  {
+    "revision": "6c3ee8e6cd371024beaf521a7ea5ed12",
+    "url": "/kingpin/static/media/harlequins_crown.6c3ee8e6.png"
+  },
+  {
+    "revision": "2c828d6203837d1834debeaa249647e9",
+    "url": "/kingpin/static/media/havensworth.2c828d62.png"
+  },
+  {
+    "revision": "2a883f33fca3f22e5042c4f109fd1da6",
+    "url": "/kingpin/static/media/horologium.2a883f33.png"
+  },
+  {
+    "revision": "273e685385ccf417bfa995ab25e8ab42",
+    "url": "/kingpin/static/media/joker_icon.273e6853.png"
+  },
+  {
+    "revision": "bad910d604f9c449d5b645067f3d3718",
+    "url": "/kingpin/static/media/joker_iconRed.bad910d6.png"
+  },
+  {
+    "revision": "e47460648907f85d34e565159a9ca956",
+    "url": "/kingpin/static/media/karable.e4746064.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/kayn_card.1360c3dd.png"
+  },
+  {
+    "revision": "468d587cf4735dd0dbf6d9ecc95d7604",
+    "url": "/kingpin/static/media/kayn_fullbody.468d587c.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/kayn_icon.1360c3dd.png"
+  },
+  {
+    "revision": "3f17664ede24b580348cbe0e71c800f0",
+    "url": "/kingpin/static/media/kayn_icon.3f17664e.svg"
+  },
+  {
+    "revision": "9f868939c23056cc9e0c6f6815cd198b",
+    "url": "/kingpin/static/media/kayn_icon_back.9f868939.svg"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/kiyone_card.1360c3dd.png"
+  },
+  {
+    "revision": "88c3c66481a9b96a7d63f87f8fe83c2a",
+    "url": "/kingpin/static/media/kiyone_fullbody.88c3c664.png"
+  },
+  {
+    "revision": "024e65c6b93dbd0722ed5012bb7b4125",
+    "url": "/kingpin/static/media/kiyone_icon.024e65c6.png"
+  },
+  {
+    "revision": "9ad36d608d085259d35752037749c6a0",
+    "url": "/kingpin/static/media/kiyone_icon_back.9ad36d60.svg"
+  },
+  {
+    "revision": "5e3026f8ddc8924c62138c402c5cd8e2",
+    "url": "/kingpin/static/media/koranin_temp.5e3026f8.jpg"
+  },
+  {
+    "revision": "64482c6a845246f56c40d38eb977f490",
+    "url": "/kingpin/static/media/kozen_shrine.64482c6a.png"
+  },
+  {
+    "revision": "4201927089770fe15c03cc06a2a0c44d",
+    "url": "/kingpin/static/media/laleh_card.42019270.png"
+  },
+  {
+    "revision": "23430c15544eafc86461ab91a361a822",
+    "url": "/kingpin/static/media/laleh_fullbody.23430c15.png"
+  },
+  {
+    "revision": "bcb9c086e94c9678c53cab6b03ea8945",
+    "url": "/kingpin/static/media/laleh_icon.bcb9c086.png"
+  },
+  {
+    "revision": "6be046fa3d16de63e5b900c43648863f",
+    "url": "/kingpin/static/media/laleh_icon_back.6be046fa.svg"
+  },
+  {
+    "revision": "428506728ec23359afadd18525ab1fd8",
+    "url": "/kingpin/static/media/leaves_of_dreamers_songs.42850672.png"
+  },
+  {
+    "revision": "f51fcb45b7c73334d5d5df580a317708",
+    "url": "/kingpin/static/media/leikhazi_gardens.f51fcb45.png"
+  },
+  {
+    "revision": "547d5c99ed5340c8501943c6d9069326",
+    "url": "/kingpin/static/media/leviathans_maw.547d5c99.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/lirah_card.1360c3dd.png"
+  },
+  {
+    "revision": "468d587cf4735dd0dbf6d9ecc95d7604",
+    "url": "/kingpin/static/media/lirah_fullbody.468d587c.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/lirah_icon.1360c3dd.png"
+  },
+  {
+    "revision": "3f17664ede24b580348cbe0e71c800f0",
+    "url": "/kingpin/static/media/lirah_icon.3f17664e.svg"
+  },
+  {
+    "revision": "05bf4afb524c2917a878118f33bfa186",
+    "url": "/kingpin/static/media/lirah_icon_back.05bf4afb.svg"
+  },
+  {
+    "revision": "163be89d60643fade2e0488fa8a539f9",
+    "url": "/kingpin/static/media/logo.163be89d.svg"
+  },
+  {
+    "revision": "43dc0c4fc9144ce52d68f143502582b9",
+    "url": "/kingpin/static/media/lydi_card.43dc0c4f.png"
+  },
+  {
+    "revision": "c3c12439e96e5a315e6311ea6a571b7f",
+    "url": "/kingpin/static/media/lydi_fullbody.c3c12439.png"
+  },
+  {
+    "revision": "0c26496c4d4631ee4f0dd4c7b7be9adb",
+    "url": "/kingpin/static/media/lydi_icon.0c26496c.png"
+  },
+  {
+    "revision": "b1588ee906802bda1776556592069d49",
+    "url": "/kingpin/static/media/lydi_icon_back.b1588ee9.svg"
+  },
+  {
+    "revision": "f526c7e8a472667eb1104155e5777d13",
+    "url": "/kingpin/static/media/lyrisius.f526c7e8.svg"
+  },
+  {
+    "revision": "056e67ade82b04c0bfd83d4c42c49cca",
+    "url": "/kingpin/static/media/lyrisius_icon.056e67ad.png"
+  },
+  {
+    "revision": "1fb386fba96e2b29a20711e57a4ab4fd",
+    "url": "/kingpin/static/media/lyrisius_iconRed.1fb386fb.png"
+  },
+  {
+    "revision": "f07e049368456dd0f79c2d8dab1cde81",
+    "url": "/kingpin/static/media/lyrisius_iconWhite.f07e0493.png"
+  },
+  {
+    "revision": "dc3acd0f0357957b7d0073d1731cd88b",
+    "url": "/kingpin/static/media/lysande.dc3acd0f.svg"
+  },
+  {
+    "revision": "6e9d3c627c40674c2a60dc4d7c3708c2",
+    "url": "/kingpin/static/media/lysande_icon.6e9d3c62.png"
+  },
+  {
+    "revision": "5c86b127dee1bec5f7bfadde8008a7aa",
+    "url": "/kingpin/static/media/lysande_iconRed.5c86b127.png"
+  },
+  {
+    "revision": "6dce6f265112a6d5c829a6c4026c5970",
+    "url": "/kingpin/static/media/lysande_iconWhite.6dce6f26.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/misa_card.1360c3dd.png"
+  },
+  {
+    "revision": "468d587cf4735dd0dbf6d9ecc95d7604",
+    "url": "/kingpin/static/media/misa_fullbody.468d587c.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/misa_icon.1360c3dd.png"
+  },
+  {
+    "revision": "3f17664ede24b580348cbe0e71c800f0",
+    "url": "/kingpin/static/media/misa_icon.3f17664e.svg"
+  },
+  {
+    "revision": "cddce95a7fe7285d3a81309451152fb5",
+    "url": "/kingpin/static/media/misa_icon_back.cddce95a.svg"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/nakoa_card.1360c3dd.png"
+  },
+  {
+    "revision": "6599717510aadff3576b0c0ca19b8150",
+    "url": "/kingpin/static/media/nakoa_fullbody.65997175.png"
+  },
+  {
+    "revision": "29d57dc7577dafa20d37ff6627e723c8",
+    "url": "/kingpin/static/media/nakoa_icon.29d57dc7.png"
+  },
+  {
+    "revision": "614c43937a8cbd7b4d0da3109fd88c37",
+    "url": "/kingpin/static/media/nakoa_icon_back.614c4393.svg"
+  },
+  {
+    "revision": "647ba04a653b14a40e869e7bce710bc6",
+    "url": "/kingpin/static/media/niroki_vela.647ba04a.png"
+  },
+  {
+    "revision": "6aeb16f96677d6d16b13bddfd1205aa8",
+    "url": "/kingpin/static/media/palettius.6aeb16f9.png"
+  },
+  {
+    "revision": "96d9b117ea045d36fe0a5405cf83f838",
+    "url": "/kingpin/static/media/port_of_royals.96d9b117.png"
+  },
+  {
+    "revision": "915f85f896ff0ee054e4d64aaa977510",
+    "url": "/kingpin/static/media/purblinde.915f85f8.png"
+  },
+  {
+    "revision": "192b1bc3de7611fa7183c6a7dfdc1e37",
+    "url": "/kingpin/static/media/regavia.192b1bc3.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/rin_card.1360c3dd.png"
+  },
+  {
+    "revision": "128522f8e4e69eb731e07ab13b89c1f1",
+    "url": "/kingpin/static/media/rin_fullbody.128522f8.png"
+  },
+  {
+    "revision": "4533ae4d30e9a7c9221447c74ebae96e",
+    "url": "/kingpin/static/media/rin_icon.4533ae4d.png"
+  },
+  {
+    "revision": "09515835080abd28dc976c06e2ead2be",
+    "url": "/kingpin/static/media/rin_icon_back.09515835.svg"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/robyn_card.1360c3dd.png"
+  },
+  {
+    "revision": "5a45e079dbbed6f966fad0a4affc1b40",
+    "url": "/kingpin/static/media/robyn_fullbody.5a45e079.png"
+  },
+  {
+    "revision": "4089401dc6c1318a454a3ca0ae140643",
+    "url": "/kingpin/static/media/robyn_icon.4089401d.png"
+  },
+  {
+    "revision": "811286ed848e0b3cbfbe0788370096f7",
+    "url": "/kingpin/static/media/robyn_icon_back.811286ed.svg"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/rook_card.1360c3dd.png"
+  },
+  {
+    "revision": "468d587cf4735dd0dbf6d9ecc95d7604",
+    "url": "/kingpin/static/media/rook_fullbody.468d587c.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/rook_icon.1360c3dd.png"
+  },
+  {
+    "revision": "3f17664ede24b580348cbe0e71c800f0",
+    "url": "/kingpin/static/media/rook_icon.3f17664e.svg"
+  },
+  {
+    "revision": "0e08dab411c5fa34fdd168ad6fc3c47f",
+    "url": "/kingpin/static/media/rook_icon_back.0e08dab4.svg"
+  },
+  {
+    "revision": "f27e1b9dce9684ad9314aff4194a07c1",
+    "url": "/kingpin/static/media/sohly_tair.f27e1b9d.png"
+  },
+  {
+    "revision": "a533307a79f23d69562c7768a46dcbee",
+    "url": "/kingpin/static/media/splash_alcerium.a533307a.png"
+  },
+  {
+    "revision": "e226144390417f8b50c80359bc5a52b8",
+    "url": "/kingpin/static/media/splash_castorix.e2261443.png"
+  },
+  {
+    "revision": "c25090fccb3843275b0899bdf2f30d46",
+    "url": "/kingpin/static/media/splash_kingpin.c25090fc.png"
+  },
+  {
+    "revision": "74fe68ef947c3d4dd856200278efd09d",
+    "url": "/kingpin/static/media/splash_kingpin_overlay.74fe68ef.png"
+  },
+  {
+    "revision": "9c2ac840545b7774561db312ad74a759",
+    "url": "/kingpin/static/media/splash_lyrisius.9c2ac840.png"
+  },
+  {
+    "revision": "53ef8ff97a7a956a157d7f692c532422",
+    "url": "/kingpin/static/media/splash_lysande.53ef8ff9.png"
+  },
+  {
+    "revision": "7a9937e84beaf34dbdaf0dc1eb9dd635",
+    "url": "/kingpin/static/media/splash_map.7a9937e8.png"
+  },
+  {
+    "revision": "621b687e248b8df8368c74b6c7225894",
+    "url": "/kingpin/static/media/splash_valterre.621b687e.png"
+  },
+  {
+    "revision": "dbf2b2e13a11092a3a621e829647e9c2",
+    "url": "/kingpin/static/media/sumina_card.dbf2b2e1.png"
+  },
+  {
+    "revision": "101d4cf8743e3f7de97bbff08e7629f3",
+    "url": "/kingpin/static/media/sumina_fullbody.101d4cf8.png"
+  },
+  {
+    "revision": "7c76ebcbc85047ab51e3444e78a0d168",
+    "url": "/kingpin/static/media/sumina_icon.7c76ebcb.png"
+  },
+  {
+    "revision": "a34603528929a9cc1193a6876aaffa80",
+    "url": "/kingpin/static/media/sumina_icon_back.a3460352.svg"
+  },
+  {
+    "revision": "6edc35ed9a01d0ff2aad9bb6ebca481b",
+    "url": "/kingpin/static/media/systems_temp.6edc35ed.jpg"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/taku_card.1360c3dd.png"
+  },
+  {
+    "revision": "468d587cf4735dd0dbf6d9ecc95d7604",
+    "url": "/kingpin/static/media/taku_fullbody.468d587c.png"
+  },
+  {
+    "revision": "3f17664ede24b580348cbe0e71c800f0",
+    "url": "/kingpin/static/media/taku_icon.3f17664e.svg"
+  },
+  {
+    "revision": "bd92b35433f8bcefd152777ca179cd89",
+    "url": "/kingpin/static/media/taku_icon_back.bd92b354.svg"
+  },
+  {
+    "revision": "3328134f44ca4b3a1637f97de1bdc6bd",
+    "url": "/kingpin/static/media/the_anglerfish.3328134f.png"
+  },
+  {
+    "revision": "f9d7d1f6d9fcb21689bd9c5fe4dfc99d",
+    "url": "/kingpin/static/media/the_elite_court.f9d7d1f6.png"
+  },
+  {
+    "revision": "4af15a148f4c57bf9aadfe3b7b1cd55e",
+    "url": "/kingpin/static/media/the_library_of_mokka.4af15a14.png"
+  },
+  {
+    "revision": "9b96ca7e9afe3bad3f78b91b085aec35",
+    "url": "/kingpin/static/media/the_north_star.9b96ca7e.png"
+  },
+  {
+    "revision": "7d6769f997aeae2784c6e95d05b0f899",
+    "url": "/kingpin/static/media/the_southern_sun.7d6769f9.png"
+  },
+  {
+    "revision": "4946035daf59cb20bd832cabb5327881",
+    "url": "/kingpin/static/media/title.4946035d.svg"
+  },
+  {
+    "revision": "805236ca7f3f9b6f6f5587cad99a0c09",
+    "url": "/kingpin/static/media/tohzas_end.805236ca.png"
+  },
+  {
+    "revision": "5bfa6392ec194d1cc11505f5a0d2cd6d",
+    "url": "/kingpin/static/media/tora_vigilinthe.5bfa6392.png"
+  },
+  {
+    "revision": "5fc343a77c3f6eca8ef925973040d350",
+    "url": "/kingpin/static/media/troubleshooters_den.5fc343a7.png"
+  },
+  {
+    "revision": "4f866e8e4463a17fed8b881cc1dd0e01",
+    "url": "/kingpin/static/media/valterre.4f866e8e.svg"
+  },
+  {
+    "revision": "63cfca5d4ca2022560a2d0394ed05d36",
+    "url": "/kingpin/static/media/valterre_icon.63cfca5d.png"
+  },
+  {
+    "revision": "33245003f928035768643aa3fbe8fe87",
+    "url": "/kingpin/static/media/valterre_iconRed.33245003.png"
+  },
+  {
+    "revision": "46a9a1baf30078db54d1d1baf6a02399",
+    "url": "/kingpin/static/media/valterre_iconWhite.46a9a1ba.png"
+  },
+  {
+    "revision": "2681c2550417855324940d6fac56d475",
+    "url": "/kingpin/static/media/veruphia.2681c255.png"
+  },
+  {
+    "revision": "1360c3dd7570ccc890411288837479f8",
+    "url": "/kingpin/static/media/zeretta_card.1360c3dd.png"
+  },
+  {
+    "revision": "468d587cf4735dd0dbf6d9ecc95d7604",
+    "url": "/kingpin/static/media/zeretta_fullbody.468d587c.png"
+  },
+  {
+    "revision": "039c56fc3233b648fa4b030722c4a350",
+    "url": "/kingpin/static/media/zeretta_icon.039c56fc.png"
+  },
+  {
+    "revision": "9a005c3a1199a91e13d36b3e5574069d",
+    "url": "/kingpin/static/media/zeretta_icon_back.9a005c3a.svg"
+  },
+  {
+    "revision": "249ab57057a4a29b73f78655d01403b6",
+    "url": "/kingpin/static/media/zeronyn.249ab570.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kingpin/precache-manifest.da9ee57f272ac4d8edb727a17038f0e8.js"
+  "/kingpin/precache-manifest.90ebec61c52908e5913b83f50f45b709.js"
 );
 
 self.addEventListener('message', (event) => {
